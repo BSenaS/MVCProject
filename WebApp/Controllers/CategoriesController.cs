23bdb8c60ad7f32334e126e8f0e0447847d06de7
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-11-01 17:58:04 +0300

    -Creates a HTTPOST for update Category.

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -17,5 +17,26 @@ namespace WebApp.Controllers
             var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
             return View(category);
         }
+
+        /*
+          public static void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var categoryToUpdate = GetCategoryById(categoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+         */
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
