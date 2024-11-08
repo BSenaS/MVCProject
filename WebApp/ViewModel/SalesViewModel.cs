57f798b5e72e54c3c70033b09fa4a0199fde8ae7
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-11-08 16:56:43 +0300

    -Creates SalesViewModel -Creates SalesController -Creates Sales - Categories Dropdown List

---
+++ WebApp/ViewModel/SalesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModel
+{
+    public class SalesViewModel
+    {
+        public int SelectedCategoryId { get; set; }
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+    }
+}
