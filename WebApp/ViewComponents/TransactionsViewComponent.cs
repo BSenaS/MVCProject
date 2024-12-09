8af42cc065351e2cf1fdf712f554723cf6bacf5a
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-12-09 18:11:42 +0300

    -Creates ViewComponents for transaction.

---
+++ WebApp/ViewComponents/TransactionsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    [ViewComponent]  //-> Bu attr. ile bu classın bir ViewComponent oldugu belirtiriz.
+    public class TransactionsViewComponent
+    {
+        public string Invoke()
+        {
+            return "List of Transactions";
+        }
+    }
+}
