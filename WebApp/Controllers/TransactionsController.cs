e5562a6bc70c2e11eaf221d0c8e44887b7028700
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-12-18 15:46:49 +0300

    -Creates Transaction Report Controller - View - ViewModel -Adds Link to Transaction Report -Adds Search Functionality to Transaction Report

---
+++ WebApp/Controllers/TransactionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //1.Initialize the ViewModel first.
+            TransactionsViewModel transactionsViewModel = new TransactionsViewModel();
+            return View(transactionsViewModel);
+        }
+
+        public IActionResult Search(TransactionsViewModel transactionsViewModel)
+        {
+            //1-)Filter sonucuna göre, Repositoryden ilgili verileri çekme
+            var transactions = TransactionsRepository.Search(transactionsViewModel.CashierName??string.Empty, transactionsViewModel.StartDate, transactionsViewModel.EndDate);
+
+            //2-)Gelen veriyi TransactionViewModele atama.
+            transactionsViewModel.Transactions = transactions;
+
+
+            return View("Index",transactionsViewModel);
+        }
+    }
+}
